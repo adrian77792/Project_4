7146f7a1c04d61427369d57a504d2cae42b1a108
adrian77720 <adrianszmajewski8@gmail.com>
2025-12-20 08:01:07 +0000

    Add product list view

--- core/views.py
+++ core/views.py
@@ -5,6 +5,9 @@ from products.models import Category
 
 def home(request):
     products = Product.objects.all()
+    for p in products:
+        if p.discount:
+            p.discounted_price = p.price - (p.price * p.discount / 100)
     categories = Category.objects.all()[:4]
     print (categories)
     return render(request, "core/index.html",
