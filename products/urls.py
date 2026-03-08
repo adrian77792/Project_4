5d0ceb582ed941ce2acb4a949421c08bc855d90b
adrian77720 <adrianszmajewski8@gmail.com>
2026-03-08 08:02:14 +0000

    About us fix, product page

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ app_name = "products"
 urlpatterns = [
     path("<slug:slug>/", views.product_detail, name="detail"),
     path("list", views.product_list, name="list"),
+    path("", views.product_list, name="list"),
 ]
--- products/views.py
+++ products/views.py
@@ -5,5 +5,23 @@ def product_list(request):
     return render(request, "products/product_list.html",
                   {"products":products})
 def product_detail(request, slug):
+    breadcrumbs = generate_breadcrumbs(request)
     product = get_object_or_404(Product, slug=slug)
-    return render(request, "products/product_detail.html", {"product": product})
+    return render(request, "products/product_page.html", {"product": product,"breadcrumbs": breadcrumbs } )
+
+
+def generate_breadcrumbs(request):
+    path_parts = [part for part in request.path.strip('/').split('/') if part]
+    breadcrumbs = []
+    accumulated_path = ''
+
+    for part in path_parts:
+        accumulated_path += f'/{part}'
+        name = ' '.join([w.capitalize() for w in part.replace('-', ' ').replace('_', ' ').split()])
+        breadcrumbs.append({
+            'name': name,
+            'url': accumulated_path
+        })
+    if breadcrumbs:
+        breadcrumbs[-1]['url'] = ''
+    return breadcrumbs
