da8660e0f514f0ffc024e29b9dd5f1d0d60174f8
adrian77720 <adrianszmajewski8@gmail.com>
2026-03-22 18:38:19 +0000

    fix(admin): restore adminsortable drag & drop and revert broken admin changes

--- core/views.py
+++ core/views.py
@@ -1,17 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
 from products.models import Product, Category
 from decimal import Decimal
-
-
-"""def home(request):
-    products = Product.objects.all()
-    for p in products:
-        if p.discount:
-            p.discounted_price = p.price - (p.price * p.discount / 100)
-    categories = Category.objects.all()[:6]
-    print (categories)
-    return render(request, "core/index.html",
-                  {"products":products, "categories":categories})"""
 
 
 def generate_breadcrumbs(request):
@@ -21,36 +10,39 @@ def generate_breadcrumbs(request):
 
     for part in path_parts:
         accumulated_path += f'/{part}'
-        name = ' '.join([w.capitalize() for w in part.replace('-', ' ').replace('_', ' ').split()])
+        name = ' '.join(
+            [w.capitalize() for w in part.replace('-', ' ').replace('_', ' ').split()]
+        )
         breadcrumbs.append({
             'name': name,
             'url': accumulated_path
         })
+
     if breadcrumbs:
         breadcrumbs[-1]['url'] = ''
+
     return breadcrumbs
 
+
 def home(request):
-    products = list(Product.objects.all())
     cart = request.session.get('cart', {})
     cart_count = sum(cart.values())
-    
-    latest_products = sorted(products, key=lambda p: p.id, reverse=True)[:4]
-    
- 
+
+    products = Product.objects.order_by('order', '-created_at')
+    latest_products = Product.objects.order_by('-created_at')[:4]
+
     discounted_products = []
     for p in products:
         if p.discount:
             try:
-                p.discounted_price = p.price - (p.price * p.discount / 100)
+                p.discounted_price = p.price - (p.price * p.discount / Decimal('100'))
                 discounted_products.append(p)
             except TypeError:
-                pass  # zabezpieczenie gdyby price/discount były None
+                pass
 
     discounted_products = discounted_products[:4]
 
-  
-    categories = list(Category.objects.all()[:6])
+    categories = Category.objects.order_by('order')[:6]
 
     return render(request, "core/index.html", {
         "cart_count": cart_count,
@@ -59,10 +51,12 @@ def home(request):
         "categories": categories,
     })
 
+
 def about_us(request):
     cart = request.session.get('cart', {})
     cart_count = sum(cart.values())
     breadcrumbs = generate_breadcrumbs(request)
+
     return render(request, "core/about_us.html", {
         "breadcrumbs": breadcrumbs,
         "cart_count": cart_count,
